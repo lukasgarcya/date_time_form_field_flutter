314636c40bb16f2da6657104085dbd3648c8834e
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2020-04-20 20:19:21 -0300

    Campo somente hora e somente data

--- lib/date_time_form_field.dart
+++ lib/date_time_form_field.dart
@@ -50,3 +50,70 @@ class DateTimeFormField extends FormField<DateTime> {
                       ]));
             });
 }
+
+class DateFormField extends FormField<DateTime> {
+  DateFormField(
+      {Key key,
+      Widget label,
+      DateTime initialValue,
+      DateTime firstDate,
+      DateTime lastDate,
+      Locale locale,
+      FormFieldValidator<DateTime> validator})
+      : super(
+            key: key,
+            initialValue: initialValue,
+            builder: (FormFieldState<DateTime> state) {
+              final TextEditingController controller = TextEditingController();
+              controller.text = state.value
+                  .toString(); // TODO: Usar formatação para apenas data
+              return Builder(
+                  builder: (context) => Column(children: <Widget>[
+                        label == null ? Container() : label,
+                        TextFormField(
+                            controller: controller,
+                            onTap: () async {
+                              FocusScope.of(context)
+                                  .requestFocus(new FocusNode());
+                              DateTime dateTime = await showDatePicker(
+                                  context: context,
+                                  initialDate: state.value,
+                                  firstDate: firstDate,
+                                  lastDate: lastDate,
+                                  locale: locale);
+                              if (dateTime != null) {
+                                state.didChange(dateTime);
+                              }
+                            })
+                      ]));
+            });
+}
+
+class TimeFormField extends FormField<TimeOfDay> {
+  TimeFormField({Key key, Widget label, TimeOfDay initialValue})
+      : super(
+            key: key,
+            initialValue: initialValue,
+            builder: (FormFieldState<TimeOfDay> state) {
+              final TextEditingController controller = TextEditingController();
+              controller.text = '${state.value.hour<10?'0':''}${state.value.hour}:${state.value.minute<10?'0':''}${state.value.minute}';
+              return Builder(
+                  builder: (context) => Column(children: <Widget>[
+                        label == null ? Container() : label,
+                        TextFormField(
+                            controller: controller,
+                            onTap: () async {
+                              FocusScope.of(context)
+                                  .requestFocus(new FocusNode());
+                              TimeOfDay timeOfDay = await showTimePicker(
+                                  context: context,
+                                  initialTime: TimeOfDay(
+                                      hour: state.value.hour,
+                                      minute: state.value.minute));
+                              if (timeOfDay != null) {
+                                state.didChange(timeOfDay);
+                              }
+                            })
+                      ]));
+            });
+}
